00b33ef343cfd2b66a788671ee7f8d267c2cebab
Timothy Attah <timothyattah733@gmail.com>
2026-06-08 05:27:51 +0100

    Fixing ts rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { TodoProps } from "@/types";
 import prisma from "@/utils/prisma";
 import Image from "next/image";
 
-async function getTodos() {
+async function getTodos():Promise<TodoProps[]> {
   const data = await prisma.todo.findMany({
     select: {
       title: true,
@@ -31,7 +31,7 @@ export default async function Home() {
         <AddTodo />
         <div className="flex flex-col gap-5 items-center justify-center mt-10 w-screen">
           {/* Map todos */}
-          {todos.map((todo, id): any => (
+          {todos.map((todo, id)=> (
             <div className="w-full" key={id}>
               <Todos todo={todo} />
             </div>
